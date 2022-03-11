8e63464258de13498033086e52ec442634b3dcf5
dnlbabkin <danilbabkin57@gmail.com>
2022-03-11 16:03:15 +0300

    refactor client and add xsd files

--- src/main/java/com/example/personmicroservice/account/SoapClient.java
+++ src/main/java/com/example/personmicroservice/account/SoapClient.java
@@ -8,31 +8,50 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Collections;
 import java.util.Map;
 
 @Service
 public class SoapClient {
 
-    @Autowired
-    public Jaxb2Marshaller marshaller;
-
     private RestTemplate template;
 
-    private static final String url = "http://danil-System-Product-Name:8088/mockDailyInfoSoap";
+    private static final String url = "http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx";
 
-    public ResponseEntity<AllDataInfoXMLResponse> getData (AllDataInfoXML request) {
+    public ResponseEntity<AllDataInfoXMLResponse> getData (String request) throws JAXBException {
         template = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_XML);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
+        headers.setContentType(MediaType.TEXT_XML);
+        headers.setAccept(Collections.singletonList(MediaType.TEXT_XML));
 
-        HttpEntity<Map<AllDataInfoXML, Object>> entity = new HttpEntity(request, headers);
+        AllDataInfoXML allDataInfoXML = new AllDataInfoXML();
+        StringWriter writer = new StringWriter();
 
-        ResponseEntity<AllDataInfoXMLResponse> allDataInfoXML = template
-                .exchange(url, HttpMethod.POST, entity, AllDataInfoXMLResponse.class);
+        JAXBContext jaxbContext = JAXBContext.newInstance(AllDataInfoXML.class);
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.marshal(allDataInfoXML, writer);
+        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
 
-        return allDataInfoXML;
+        HttpEntity<Map<String, Object>> entity = new HttpEntity(writer.toString(), headers);
+
+        ResponseEntity<String> response = template
+                .exchange(url, HttpMethod.POST, entity, String.class);
+
+        String responseXML = response.getBody();
+
+        JAXBContext context = JAXBContext.newInstance(AllDataInfoXMLResponse.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        AllDataInfoXMLResponse dataInfoXMLResponse = (AllDataInfoXMLResponse) unmarshaller.unmarshal(new StringReader(responseXML));
+
+        return null;
     }
 }
--- src/main/java/com/example/personmicroservice/account/SoapController.java
+++ src/main/java/com/example/personmicroservice/account/SoapController.java
@@ -1,13 +1,14 @@
 package com.example.personmicroservice.account;
 
 
-import com.example.personmicroservice.AllDataInfoXML;
 import com.example.personmicroservice.AllDataInfoXMLResponse;
 import com.example.personmicroservice.model.USD;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.xml.bind.JAXBException;
 
 @RestController
 public class SoapController {
@@ -16,7 +17,7 @@ public class SoapController {
     private SoapClient soapClient;
 
     @PostMapping(value = "/account", produces = {MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<AllDataInfoXMLResponse> invokeSoapClient(@RequestBody AllDataInfoXML request) {
+    public ResponseEntity<AllDataInfoXMLResponse> invokeSoapClient(@RequestBody String request) throws JAXBException {
         return soapClient.getData(request);
     }
 
